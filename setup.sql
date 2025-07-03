6ca2f9668dfd8b448d72dd2ac319705f2345bdb2
snow-jp-handson <tsubasa.kanno+01@snowflake.com>
2025-07-03 19:34:05 +0900

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -5,6 +5,7 @@ USE ROLE ACCOUNTADMIN;
 USE WAREHOUSE COMPUTE_WH;
 
 
+
 // Step2: 各種オブジェクトの作成 //
 
 -- データベースの作成
@@ -17,6 +18,7 @@ USE SCHEMA SNOWRETAIL_DB.SNOWRETAIL_SCHEMA;
 -- ステージの作成
 CREATE OR REPLACE STAGE SNOWRETAIL_DB.SNOWRETAIL_SCHEMA.FILE DIRECTORY = (ENABLE = TRUE);
 CREATE OR REPLACE STAGE SNOWRETAIL_DB.SNOWRETAIL_SCHEMA.SEMANTIC_MODEL_STAGE DIRECTORY = (ENABLE = TRUE);
+
 
 
 // Step3: 公開されているGitからデータとスクリプトを取得 //
@@ -33,29 +35,25 @@ CREATE OR REPLACE GIT REPOSITORY GIT_INTEGRATION_FOR_HANDSON
   ORIGIN = 'https://github.com/snow-jp-handson-org/cortex-handson-jp.git';
 
 -- チェックする
-ls @GIT_INTEGRATION_FOR_HANDSON/branches/main;
+ls @GIT_INTEGRATION_FOR_HANDSON/branches/pub_20250709;
 
 -- Githubからファイルを持ってくる
-COPY FILES INTO @SNOWRETAIL_DB.SNOWRETAIL_SCHEMA.FILE FROM @GIT_INTEGRATION_FOR_HANDSON/branches/main/data/;
-COPY FILES INTO @SNOWRETAIL_DB.SNOWRETAIL_SCHEMA.SEMANTIC_MODEL_STAGE FROM @GIT_INTEGRATION_FOR_HANDSON/branches/main/handson2/sales_analysis_model.yaml;
+COPY FILES INTO @SNOWRETAIL_DB.SNOWRETAIL_SCHEMA.FILE FROM @GIT_INTEGRATION_FOR_HANDSON/branches/pub_20250709/data/;
+COPY FILES INTO @SNOWRETAIL_DB.SNOWRETAIL_SCHEMA.SEMANTIC_MODEL_STAGE FROM @GIT_INTEGRATION_FOR_HANDSON/branches/pub_20250709/handson/sales_analysis_model.yaml;
 
-// Step4: NotebookとStreamlitを作成 //
 
--- Notebookの作成
-CREATE OR REPLACE NOTEBOOK cortex_handson_part1
-    FROM @GIT_INTEGRATION_FOR_HANDSON/branches/main/handson1
-    MAIN_FILE = 'cortex_handson_seminar_part1.ipynb'
-    QUERY_WAREHOUSE = COMPUTE_WH
-    WAREHOUSE = COMPUTE_WH;
+
+// Step4: Streamlitを作成 //
 
 -- Streamlit in Snowflakeの作成
 CREATE OR REPLACE STREAMLIT sis_snowretail_analysis_dev
-    FROM @GIT_INTEGRATION_FOR_HANDSON/branches/main/handson2/dev
+    FROM @GIT_INTEGRATION_FOR_HANDSON/branches/pub_20250709/handson/dev
     MAIN_FILE = 'mainpage.py'
     QUERY_WAREHOUSE = COMPUTE_WH;
 
 -- (Option) MVP版のStreamlit in Snowflakeの作成
+-- 完成版のアプリを動かしたい場合は以下のコメントアウトを外し実行してください
 -- CREATE OR REPLACE STREAMLIT sis_snowretail_analysis_mvp
---     FROM @GIT_INTEGRATION_FOR_HANDSON/branches/main/handson2/mvp
+--     FROM @GIT_INTEGRATION_FOR_HANDSON/branches/pub_20250709/handson/mvp
 --     MAIN_FILE = 'mainpage.py'
 --     QUERY_WAREHOUSE = COMPUTE_WH;
